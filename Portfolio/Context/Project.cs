e9661ab8e752c243e93b41baa5892a187f749fec
psg107 <psg107@naver.com>
2021-12-09 01:38:36 +0900

    프로젝트 씨드를 외부 json 파일 기반으로 생성하도록 변경

---
+++ Portfolio/Context/Project.cs
@@ -0,0 +1,23 @@
+﻿using Portfolio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Context
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public List<string> ProjectType { get; set; }
+        public string Description { get; set; }
+        public int? CreateYear { get; set; }
+        public string SourceUrl { get; set; }
+        public bool IsHiddenSourceUrl { get; set; }
+        public string ReferenceUrl { get; set; }
+        public string ImageFilePath { get; set; }
+        public bool IsHidden { get; set; }
+        public List<string> Skills { get; set; } = new List<string>();
+    }
+}
